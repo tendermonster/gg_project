f247abccc3ce4dff12b17ce253ae5a8dd5837eab
tendermonster <artiom.blinovas@gmail.com>
2021-11-25 14:55:43 +0100

    removing some unneded comments

--- microgrid/strategy.py
+++ microgrid/strategy.py
@@ -10,7 +10,6 @@ class Strategy:
 
     def utility(self,s,grid):
         """
-            x - amount to buy,sell
             s - 0, buy 1,sell
             grid - grid
         """
@@ -40,46 +39,6 @@ class Strategy:
                 location = np.argwhere(subtable == np.amin(subtable))[0].flatten()
                 return {BUYING: location}
 
-    """
-    def _comb_strategies(self,m:Microgrid):
-       
-        Combinatorics of all possible strategies players can play
-       
-        i:Player
-        space_strategies = []
-        for i in m.players:
-            space_strategies.append(i.possible_strategies())
-
-        space_strategies = list(itertools.product(*space_strategies))
-        return space_strategies
-    """
-    """
-    def _utility_function(self,m:Microgrid, strategy):
-    
-        Returns the utility
-        1D - Value depends on player state
-        utility = 1D.[$Sell $Buy $Store]
-     
-        i:Player
-        for i in m.players:
-            # set the strategy for the player i
-            i.state = strategy[i.id]
-        total_transac = m.amount_SellBuy()
-        gain_storage = m.amount_gainStorage()
-        utility = total_transac + gain_storage
-        return utility
-    """
-    """
-    def _strategies_utilities(self,m:Microgrid):
-  
-        Computes utilities of each set of strategies
-      
-        space_strategies = self._comb_strategies(m)
-        calc_utilities = []
-        for s in space_strategies:
-            calc_utilities.append(self._utility_function(m,s))
-        return np.array(calc_utilities)
-    """
     class Choice:
         GT = 0
         ALWAYS_BUY = 1
